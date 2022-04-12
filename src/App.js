e73b6d4168bf6959b59b377effac643201545973
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-04-12 04:45:34 +0300

    Patika fixed on page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Patika from './components/Patika';
+import  useWindowSize  from './hooks/useWindowSize';
 
 function App() {
+  const [, height] = useWindowSize();
+
   return (
     <div className="App">
-      
-      
+      <Patika   height={height} />
+      <div className='form'></div>
     </div>
   );
 }
