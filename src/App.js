21f933e55c8dc1e92e78ebd66b1f1852525971e0
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-04-14 02:24:51 +0300

    Project is ready

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 
 import { useState } from 'react';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import FormSide from './components/Form';
 import Patika from './components/Patika';
 import  useWindowSize  from './hooks/useWindowSize';
 
 function App() {
-  const [, height] = useWindowSize();
-  const [night, setNight] = useState(false)
+  const [, height] = useWindowSize(); // ekran boyutunu veren custom hook yapısı
+  const [night, setNight] = useState(false) // gece modu
 
   return (
     <div className="App">
       <Patika   height={height} isNight = {night} />
       <FormSide isNight={night} change={setNight}/>
+      <ToastContainer />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,29 @@
-import React from "react";
-import Moon from "../icons/Moon";
-import Sun from "../icons/Sun";
+import React, { useState } from "react";
 import { Formik } from "formik";
-import * as Yup from "yup";
+import clsx from "clsx";
+import { toast } from 'react-toastify';
+import Moon from "../icons/Moon"; // ay componenti
+import Sun from "../icons/Sun"; // gunes componenti
+import { formSchema } from "../constants/formSchema"; // yup scheması
+import LoadingSvg from "../constants/loading"; // loading gorseli icin component
+
 
 function FormSide({ isNight, change }) {
-  const formSchema = Yup.object().shape({
-    email: Yup
-    .string()
-    .email("Lütfen geçerli bir email adresi giriniz.")
-    .required("Eposta alanı zorunludur."),
-    password: Yup
-    .string()
-    .typeError("Her karakteri kullanamazsınız.")
-    .min(8, "En az 8 karakter olmalıdır.")
-    .max(32, "En fazla 32 karakter olmalıdır.")
-    .required("Şifre alanı zorunludur."),
+  
+  const [loading, setLoading] = useState(false);
+  const [isAuth, setIsAuth] = useState(false); // login olup olmadıgını kontrol ediyor
+  const [username, setUsername] = useState(''); // hosgeldiniz mesaji icin
 
-  });
+  const setServer = (auth) => {  // serverdan gelen auth bilgilerini kullanıyoruz
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setIsAuth(true);
+      toast.success('Başarılı bir şekilde kayıt oldunuz', 2000);
+    }, 3000);
+    setUsername(auth.userName); 
+  }
+  
 
   return (
     <div className={isNight ? "form-2" : "form-1"}>
@@ -32,20 +38,31 @@ function FormSide({ isNight, change }) {
           className="header-border"
           style={{ backgroundColor: isNight && "#FFBF5E" }}
         ></p>
-
-        <Formik
-          initialValues={{firstName:'', lastName:'',email: "", userName:'', password: "", passwordCheck: ""}}
+        
+        {
+          isAuth ? 
+          <div style={{ color: isNight && "#FEFEFE" }}>Hoşgeldin {username}</div>
+          :<Formik
+          initialValues={{firstName:'', lastName:'',email: "", userName:'', password: "", passwordCheck: "",checkbox: false}}
+          onSubmit = {auth => {
+            setServer(auth)
+          }}
+          validationSchema={formSchema}
         >
           {
-            ({values}) => (
-              <form className="form">
+            ({values, handleChange, handleSubmit,errors}) => (
+              <form className="form" >
             <div className="name-area">
-              <div>
+              <div >
                 <label className="title" style={{ color: isNight && "#FEFEFE" }}>İSİM</label>
                 <input 
                 type="text" 
                 name="firstName" 
-                placeholder="İsmini gir" value={values.firstName}/>
+                placeholder="İsmini gir"
+                value={values.firstName}
+                onChange={handleChange}
+                />
+                
                 <span></span>
               </div>
 
@@ -56,62 +73,72 @@ function FormSide({ isNight, change }) {
                   name="lastName"
                   placeholder="Soyisimini gir"
                   value={values.lastName}
+                  onChange={handleChange}
                 />
                 <span></span>
               </div>
             </div>
 
-            <div>
+            <div className={clsx('', {'formError': !!errors.email})}>
               <label className="required title" style={{ color: isNight && "#FEFEFE" }}>E-POSTA</label>
               <input
                 type="text"
                 name="email"
                 placeholder="E-posta adresini gir"
                 value={values.email}
+                onChange={handleChange}
               />
-              <span></span>
+              <span>{errors.email}</span>
             </div>
 
-            <div>
+            <div className={clsx('', {'formError': !!errors.userName})}>
               <label className="required title" style={{ color: isNight && "#FEFEFE" }}>KULLANICI ADI</label>
               <input
                 type="text"
                 name="userName"
                 placeholder="Kullanıcı adını gir"
                 value={values.userName}
+                onChange={handleChange}
               />
-              <span></span>
+              <span>{errors.userName}</span>
             </div>
 
-            <div>
+            <div className={clsx('', {'formError': !!errors.password})}>
               <label className="required title" style={{ color: isNight && "#FEFEFE" }}>ŞİFRE</label>
               <input
                 type="password"
                 name="password"
                 placeholder="Şifreni gir"
                 value={values.password}
+                onChange={handleChange}
               />
-              <span></span>
+              <span>{errors.password}</span>
             </div>
 
-            <div>
+            <div className={clsx('', {'formError': !!errors.passwordCheck})}>
               <label className="required title" style={{ color: isNight && "#FEFEFE" }}>ŞİFRENİ TEKRAR GİR</label>
               <input
                 type="password"
-                name="password"
+                name="passwordCheck"
                 placeholder="Şifreni doğrula"
                 value={values.passwordCheck}
+                onChange={handleChange}
               />
-              <span></span>
+              <span>{errors.passwordCheck}</span>
             </div>
 
             <div className="checkbox-area" >
-              <input type="checkbox" />
+              <input type="checkbox" value={values.checkbox} onChange={() => values.checkbox = !values.checkbox}   />
               <label className="contract">Sözleşmeyi kabul ediyorum</label>
+              <span>{errors.checkbox}</span>
             </div>
 
             <div className="formButton" >
-              <button className="registerButton" type="submit" style={{ backgroundColor: isNight && "#FFBF5E" }}>KAYIT OL</button>
+              <button className="registerButton" type="submit" onClick={handleSubmit} disabled={loading}>
+              {
+                        loading ? <LoadingSvg size={50} /> : 'KAYIT OL'
+                      }
+                      </button>
               <span></span>
             </div>
           </form>
@@ -119,6 +146,8 @@ function FormSide({ isNight, change }) {
           }
           
         </Formik>
+        }
+        
       </div>
     </div>
   );
