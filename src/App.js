501ca9729bfb93ac4f620b91d80c06a17752b4ec
Enes <66560757+devenes@users.noreply.github.com>
2022-05-13 01:37:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import { useState } from 'react';
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import FormSide from './components/Form';
 import Patika from './components/Patika';
-import  useWindowSize  from './hooks/useWindowSize';
+import useWindowSize from './hooks/useWindowSize';
 
 function App() {
   const [, height] = useWindowSize(); // ekran boyutunu veren custom hook yapısı
@@ -13,8 +13,8 @@ function App() {
 
   return (
     <div className="App">
-      <Patika   height={height} isNight = {night} />
-      <FormSide isNight={night} change={setNight}/>
+      <Patika height={height} isNight={night} />
+      <FormSide isNight={night} change={setNight} />
       <ToastContainer />
     </div>
   );
