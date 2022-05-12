fa8011b81a92588e07f001e83beaab2f18fb685a
Enes <66560757+devenes@users.noreply.github.com>
2022-05-13 01:52:15 +0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -6,5 +6,5 @@ systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
 newgrp docker
-docker pull devenes/formik-registration:1
-docker run -d -p 80:3000 --name formik-registration devenes/formik-registration:1
+docker pull devenes/formik-registration:2
+docker run -d -p 80:3000 --name formik-registration devenes/formik-registration:2
