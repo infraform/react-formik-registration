4ddaff6229627fdd5b204fa77a630f74e2f3ef9d
Enes <66560757+devenes@users.noreply.github.com>
2022-05-13 01:45:44 +0300

    🌐 Create user data file
    🌐 Create user data file

---
+++ user-data.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+yum update -y
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
+newgrp docker
+docker pull devenes/formik-registration:1
+docker run -d -p 80:3000 --name formik-registration devenes/formik-registration:1
