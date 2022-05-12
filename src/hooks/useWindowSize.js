6af58fb5167f0c79cbe6360f950bd3cc16b84732
Enes <66560757+devenes@users.noreply.github.com>
2022-05-13 01:37:12 +0300

    Update useWindowSize.js

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,4 +1,7 @@
-import { useLayoutEffect, useState } from "react";
+import {
+  useLayoutEffect,
+  useState
+} from "react";
 
 const useWindowSize = () => {
   const [width, setWidth] = useState();
@@ -8,15 +11,15 @@ const useWindowSize = () => {
     const updateSize = () => {
       setWidth(window.innerWidth);
       setHeight(window.innerHeight);
-    }
+    };
 
     updateSize();
 
-    window.addEventListener('resize', updateSize);
-    return () => window.addEventListener('resize', updateSize);
+    window.addEventListener("resize", updateSize);
+    return () => window.addEventListener("resize", updateSize);
   }, []);
 
   return [width, height];
-}
+};
 
 export default useWindowSize;
