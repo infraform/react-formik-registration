ef5666d769b4eb536f98be4ffd3d7f7d822b33bf
Enes <66560757+devenes@users.noreply.github.com>
2022-05-13 01:37:09 +0300

    Update formSchema.js

--- src/constants/formSchema.js
+++ src/constants/formSchema.js
@@ -3,25 +3,25 @@ import * as Yup from "yup";
 
 export const formSchema = Yup.object().shape({
     email: Yup
-    .string()
-    .email("Lütfen geçerli bir email adresi giriniz.")
-    .required("Eposta alanı zorunludur."),
+        .string()
+        .email("Lütfen geçerli bir email adresi giriniz.")
+        .required("Eposta alanı zorunludur."),
     password: Yup
-    .string()
-    .typeError("Her karakteri kullanamazsınız.")
-    .min(8, "En az 8 karakter olmalıdır.")
-    .max(32, "En fazla 32 karakter olmalıdır.")
-    .required("Şifre alanı zorunludur."),
+        .string()
+        .typeError("Her karakteri kullanamazsınız.")
+        .min(8, "En az 8 karakter olmalıdır.")
+        .max(32, "En fazla 32 karakter olmalıdır.")
+        .required("Şifre alanı zorunludur."),
     userName: Yup
-    .string()
-    .min(3, "En az 3 karakter olmalıdır.")
-    .max(32, "En fazla 32 karakter olmalıdır.")
-    .required("Kullanıcı adı alanı zorunludur."),
+        .string()
+        .min(3, "En az 3 karakter olmalıdır.")
+        .max(32, "En fazla 32 karakter olmalıdır.")
+        .required("Kullanıcı adı alanı zorunludur."),
     passwordCheck: Yup
-    .string()
-    .oneOf([Yup.ref("password"), null], "Şifreler uyuşmuyor.")
-    .required("Şifre alanı zorunludur."),
+        .string()
+        .oneOf([Yup.ref("password"), null], "Şifreler uyuşmuyor.")
+        .required("Şifre alanı zorunludur."),
     checkbox: Yup
-    .boolean()
-    .oneOf([true], 'Sözleşmeyi kabul etmelisiniz')
+        .boolean()
+        .oneOf([true], 'Sözleşmeyi kabul etmelisiniz')
 });
